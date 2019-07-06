2d5514a10baae1c706facaeb626809b14aa09bf2
dimo <li9lnt@gmail.com>
2019-07-06 16:57:54 +0900

    regist

---
+++ view/src/components/DatePickModal.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-dialog
+        ref="dialog"
+        v-model="modal"
+        :return-value.sync="date"
+        persistent
+        lazy
+        full-width
+        width="290px"
+    >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+            v-model="date"
+            label="日付"
+            prepend-icon="event"
+            readonly
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="date" color="teal">
+          <v-spacer></v-spacer>
+          <v-btn flat color="grey" @click="modal = false">キャンセル</v-btn>
+          <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
+        </v-date-picker>
+      </v-dialog>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      date: new Date().toISOString().substr(0, 10),
+      modal: false
+    })
+  }
+</script>
---
+++ view/src/components/DatesPickList.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <v-list two-line>
+        <template v-for="(dates, i) in dates">
+            <v-list-tile v-bind:key="i">
+                <v-list-tile-content>
+                    <DatePickModal/>
+                </v-list-tile-content>
+                <!--v-list-tile-action>
+                    <v-btn flat icon v-on:click="deleteDate(i)">
+                        <v-icon>delete</v-icon>
+                    </v-btn>
+                </v-list-tile-action-->
+            </v-list-tile>
+        </template>
+    </v-list>
+    <div class="bittom-right">
+        <v-btn dark color="primary" v-on:click="addDate(i)">
+            日付を追加する
+        </v-btn>
+    </div>
+</div>
+</template>
+
+
+<script>
+import DatePickModal from "./DatePickModal"
+
+    export default {
+        data: () => ({
+            dates: ['']
+        }),
+        methods: {
+            deleteDate(i) {
+                this.dates.splice(i, 1);
+            },
+            addDate(i){
+                this.dates.push("日付を追加する");
+            }
+        },
+        components: {
+            DatePickModal
+        }
+    }
+</script>
+
