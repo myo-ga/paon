25c2aa288d9ad01db5174c310138ebd1dace7dbd
ya <hr.yahagi@gmail.com>
2021-03-02 22:48:56 +0900

    event.jsの/event/updateに対するテスト追加

--- server/tests/event.test.js
+++ server/tests/event.test.js
@@ -523,7 +523,358 @@ describe("Integration Test", () => {
         else  done();
       })
     });
-
-
   });
+
+
+  describe("POST /event/update", () => {
+
+    var event_id = "";
+    var event_rev = "";
+
+    beforeEach(async () => {
+      // 前処理として、db削除テスト用途に1レコード登録する
+      event = {
+        eventName: "飲み会",
+        eventMemo: "のみましょう",
+        eventAddDays: "2021-01-01 18:00,2021-01-01 21:00",
+        storeId: "1234567890",
+        storeLatitude: "23.12",
+        storeLongitude: "135.12",
+        storeName: "鳥貴族",
+        storeAddress: "東京都新宿区東口",
+        storeUrl: "http://sp.torikizoku.co.jp"
+      };
+      try {
+        let response = await request(app).post("/event/create").send(event);
+        if (response.body.ok === false) {
+          throw new Error("response ok field fail.");
+        }
+        event_id = response.body.id;
+        event_rev = response.body.rev;
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    afterAll(async () => {
+      // 後処理として、db削除用途に登録したレコードを削除する
+      try {
+        let event = {
+          id: event_id,
+          rev: event_rev
+        };
+        let response = await request(app).post("/event/delete").send(event);
+        if (response.body.ok === false) {
+          throw new Error("response ok field fail.");
+        }
+      } catch (err) {
+        console.log("A test record of /event/delete is already deleted !");
+      }
+    });
+
+    test("idの文字数超過でバリデーションに失敗する", (done) => {
+      event.id = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      postValidateFailTest(done, "/event/update", "id");
+    });
+
+    test("revの文字数超過でバリデーションに失敗する", (done) => {
+      event.id = event_id;
+      event.rev = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      event.eventDelDays = "day0,day1";
+      postValidateFailTest(done, "/event/update", "rev");
+    });
+
+    test("eventNameの文字数超過でバリデーションに失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.eventName = "のみかいをやります。。あいうえおかきくけこさしすせそた０１２３４５６７８９abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'())-=^~|[{}]`*:;+,<.>/_";
+      postValidateFailTest(done, "/event/update", "eventName");
+    });
+
+    test("eventMemoの文字数超過でバリデーションに失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.eventMemo = "のみかいをやります。。あいうえおかきくけこさしすせそた０１２３４５６７８９abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'())-=^~|[{}]`*:;+,<.>/_";
+      postValidateFailTest(done, "/event/update", "eventMemo");
+    });
+
+    // eventAddDaysのバリデーションエラー
+    test("eventAddDaysのフォーマットエラーによりバリデーションが失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.eventAddDays = "2021/01/01 18:00,2021/01/01 19:00";
+      postValidateFailTest(done, "/event/update", "eventAddDays");
+    });
+
+    test("eventAddDaysの存在しない日付によりバリデーションが失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.eventAddDays = "2021/13/01 18:00,2021/01/01 19:00";
+      postValidateFailTest(done, "/event/update", "eventAddDays");
+    });
+
+    test("eventAddDaysの未定義によりバリデーションが失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      delete event.eventAddDays;
+      postValidateFailTest(done, "/event/update", "eventAddDays");
+    });
+
+    // eventDelDaysのバリデーションエラー
+    test("eventDelDaysのフォーマットエラーによりバリデーションに失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "abcdefg";
+      postValidateFailTest(done, "/event/update", "eventDelDays");
+    });
+
+    test("eventDelDaysの未定義によりバリデーションに失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      postValidateFailTest(done, "/event/update", "eventDelDays");
+    });
+
+
+    // storeIdのバリデーションエラー
+    test("storeIdの文字数超過によりバリデーションが失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.storeId = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      postValidateFailTest(done, "/event/update", "storeId");
+    });
+
+    // storeLatitudeのバリデーションエラー
+    test("storeLatitudeの文字数超過によりバリデーションが失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.storeLatitude = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      postValidateFailTest(done, "/event/update", "storeLatitude");
+    });
+
+    // storeLongitudeのバリデーションエラー
+    test("storeLongitudeの文字数超過によりバリデーションが失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.storeLongitude = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      postValidateFailTest(done, "/event/update", "storeLongitude");
+    });
+
+    // storeNameのバリデーションエラー
+    test("storeNameの文字数超過によりバリデーションが失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.storeName = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      postValidateFailTest(done, "/event/update", "storeName");
+    });
+
+    // storeAddressのバリデーションエラー
+    test("storeAddressの文字数超過によりバリデーションが失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.storeAddress = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      postValidateFailTest(done, "/event/update", "storeAddress");
+    });
+
+    // storeUrlのバリデーションエラー
+    test("storeUrlの文字数超過によりバリデーションが失敗する", (done) => {
+      event.id = event_id;
+      event.rev = event_rev;
+      event.eventDelDays = "day0,day1";
+      event.storeUrl = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      postValidateFailTest(done, "/event/update", "storeUrl");
+    });
+
+    // id空による更新失敗
+    test("idが空のため、dbの更新に失敗する", (done) => {
+      let data = {
+        id: "",
+        rev: event_rev,
+        eventName: "忘年会",
+        eventMemo: "ぼうねんかいです",
+        eventAddDays: "2021-12-31 18:00,2021-12-31 21:00",
+        eventDelDays: "day0,day1",
+        storeId: "1234567890",
+        storeLatitude: "23.12",
+        storeLongitude: "135.12",
+        storeName: "鳥貴族",
+        storeAddress: "東京都新宿区東口",
+        storeUrl: "http://sp.torikizoku.co.jp"
+      };
+      request(app).post("/event/update")
+      .send(data)
+      .expect(422)
+      .expect((res) => {
+        let ret = {
+          ok: false,
+          type: "record update error",
+          errors: [{msg: "Empty id"}]
+        }
+        expect(res.body).toEqual(ret);
+      })
+      .end((err, res) => {
+        if (err) {
+          console.log(res);
+          return done(err);
+        }
+        else  done();
+      });
+    });
+
+    // db更新時にエラー
+    test("dbのイベント更新時にエラーが発生する", (done) => {
+
+      let model = require("../model/couchdb.js");
+      let spy = jest.spyOn(model.DB.prototype, "updateOneRecord").mockImplementation((data, param) => {
+        throw new Error("db update error.");
+      });
+
+      let data = {
+        id: event_id,
+        rev: event_rev,
+        eventName: "忘年会",
+        eventMemo: "ぼうねんかいです",
+        eventAddDays: "2021-12-31 18:00,2021-12-31 21:00",
+        eventDelDays: "day0,day1",
+        storeId: "1234567890",
+        storeLatitude: "23.12",
+        storeLongitude: "135.12",
+        storeName: "鳥貴族",
+        storeAddress: "東京都新宿区東口",
+        storeUrl: "http://sp.torikizoku.co.jp"
+      };
+      request(app).post("/event/update")
+      .send(data)
+      .expect(422)
+      .expect((res) => {
+        expect(res.body.ok).toBe(false);
+        expect(res.body.type).toBe("record update error");
+      })
+      .end((err, res) => {
+        spy.mockRestore();
+        if (err) {
+          console.log(res);
+          return done(err);
+        }
+        else  done();
+      });
+    });
+
+    // dbのイベント更新に成功する
+    test("dbの更新に成功する", async () => {
+
+      // メンバーを追加1
+      let data = {
+        id: event_id,
+        memberName: "Duo",
+        memberComment: "wryyyy",
+        day0: "OK",
+        day1: "OK"
+      };
+      let response = await request(app).post("/member/create")
+      .send(data)
+      .expect(200);
+
+      // メンバーを追加2
+      data = {
+        id: event_id,
+        memberName: "Johtaro",
+        memberComment: "oraoraora",
+        day0: "NG",
+        day1: "UnKnown"
+      };
+      response = await request(app).post("/member/create")
+      .send(data)
+      .expect(200)
+      .expect((res) => {
+        event_id = res.body.id;
+        event_rev = res.body.rev;
+      });
+      
+      // イベント更新処理
+      data = {
+        id: event_id,
+        rev: event_rev,
+        eventName: "忘年会",
+        eventMemo: "ぼうねんかいです",
+        eventAddDays: "2021-12-31 18:00,2021-12-31 21:00",
+        eventDelDays: "day0",
+        storeId: "1234567890",
+        storeLatitude: "23.12",
+        storeLongitude: "135.12",
+        storeName: "鳥貴族",
+        storeAddress: "東京都新宿区東口",
+        storeUrl: "http://sp.torikizoku.co.jp"
+      };
+      response = await request(app).post("/event/update")
+      .send(data)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.ok).toBe(true);
+        event_id = res.body.id;
+        event_rev = res.body.rev;
+      });
+
+      // 更新確認
+      data = {id: event_id};
+      response = await request(app).get("/event/get")
+      .query(data)
+      .expect(200)
+      .expect((res) => {
+        let ret_event = {
+          ok: true,
+          id: event_id,
+          rev: event_rev,
+          eventName: "忘年会",
+          eventMemo: "ぼうねんかいです",
+          eventDays: {
+            day1: "2021-01-01 21:00",
+            day2: "2021-12-31 18:00",
+            day3: "2021-12-31 21:00"
+          },
+          eventMembers: {
+            member0: {
+              memberName: "Duo",
+              memberComment: "wryyyy",
+              memberDays: {
+                day1: "OK",
+                day2: "None",
+                day3: "None"
+              }
+            },
+            member1: {
+              memberName: "Johtaro",
+              memberComment: "oraoraora",
+              memberDays: {
+                day1: "UnKnown",
+                day2: "None",
+                day3: "None"
+              }
+            }
+          },
+          storeId: "1234567890",
+          storeLatitude: "23.12",
+          storeLongitude: "135.12",
+          storeName: "鳥貴族",
+          storeAddress: "東京都新宿区東口",
+          storeUrl: "http://sp.torikizoku.co.jp"
+        };
+        expect(res.body).toEqual(ret_event);
+      });
+
+    });
+    
+  });
+
 });
