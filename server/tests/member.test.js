12a8b44c334c0f73d30871d9a1509d0e7df30d1b
ya <hr.yahagi@gmail.com>
2021-03-04 22:35:26 +0900

    member.jsの/member/deleteに対するテスト追加

--- server/tests/member.test.js
+++ server/tests/member.test.js
@@ -4,6 +4,52 @@ var member = {};
 
 describe("Integration Test", () => {
 
+  var event_id = "";
+  var event_rev = "";
+
+  beforeAll(async () => {
+    try {
+      let data = {
+        eventName: "飲み会",
+        eventMemo: "のみましょう",
+        eventAddDays: "2021-01-01 18:00,2021-01-01 21:00,2021-01-02 20:00,2021-01-03 19:00",
+        storeId: "1234567890",
+        storeLatitude: "23.12",
+        storeLongitude: "135.12",
+        storeName: "鳥貴族",
+        storeAddress: "東京都新宿区東口",
+        storeUrl: "http://sp.torikizoku.co.jp"
+      };
+      let response = await request(app).post("/event/create").send(data);
+      if (response.body.ok === false) {
+        throw new Error("response ok failed.");
+      }
+      event_id = response.body.id;
+      event_rev = response.body.rev;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  afterAll(async () => {
+    try {
+      let data = {
+        id: event_id
+      };
+      let response = await request(app).get("/event/get").query(data);
+      data = {
+        id: response.body.id,
+        rev: response.body.rev
+      };
+      response = await request(app).post("/event/delete").send(data);
+      if (response.body.ok === false) {
+        throw new Error("response ok failed.");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
   function postValidateFailTest(done, uri, field) {
     request(app).post(uri)
     .send(member)
@@ -22,51 +68,7 @@ describe("Integration Test", () => {
   }
 
   describe("POST /member/create", () => {
-    var event_id = "";
-    var event_rev = "";
 
-    beforeAll(async () => {
-      try {
-        let data = {
-          eventName: "飲み会",
-          eventMemo: "のみましょう",
-          eventAddDays: "2021-01-01 18:00,2021-01-01 21:00,2021-01-02 20:00,2021-01-03 19:00",
-          storeId: "1234567890",
-          storeLatitude: "23.12",
-          storeLongitude: "135.12",
-          storeName: "鳥貴族",
-          storeAddress: "東京都新宿区東口",
-          storeUrl: "http://sp.torikizoku.co.jp"
-        };
-        let response = await request(app).post("/event/create").send(data);
-        if (response.body.ok === false) {
-          throw new Error("response ok failed.");
-        }
-        event_id = response.body.id;
-        event_rev = response.body.rev;
-      } catch (err) {
-        console.log(err);
-      }
-    });
-
-    afterAll(async () => {
-      try {
-        let data = {
-          id: event_id
-        };
-        let response = await request(app).get("/event/get").query(data);
-        data = {
-          id: response.body.id,
-          rev: response.body.rev
-        };
-        response = await request(app).post("/event/delete").send(data);
-        if (response.body.ok === false) {
-          throw new Error("response ok failed.");
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    });
 
     beforeEach(() => {
       member = {
@@ -238,12 +240,163 @@ describe("Integration Test", () => {
   });
 
 
+  
   describe("POST /member/update", () => {
 
   });
 
 
-  describe("POST /member/delete", () => {
 
+  describe("POST /member/delete", () => {
+    beforeAll(async () => {
+      let data = {
+        id: event_id,
+        memberName: "田中",
+        memberComment: "たなか",
+        day0: "OK",
+        day1: "NG",
+        day2: "UnKnown",
+        day3: "None"
+      };
+      let response = await request(app).post("/member/create").send(data);
+      if (response.body.ok === false) {
+        throw new Error("/member/create beforeAll fail.");
+      }
+    });
+
+    beforeEach(() => {
+      member = {};
+    });
+
+    test("idが文字数超過のためバリデーションに失敗する", (done) => {
+      let id = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      member.id = id;
+      member.memberId = "member0";
+      postValidateFailTest(done, "/member/update", "id");
+    });
+
+    test("memberIdが文字数超過のためバリデーションに失敗する", (done) => {
+      let memberId = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+      member.id = event_id;
+      member.memberId = memberId;
+      postValidateFailTest(done, "/member/update", "memberId");
+    });
+
+    test("idが空のためdbのイベントレコード取得に失敗する", (done) => {
+      member.id = "";
+      member.memberId = "member0";
+      request(app).post("/member/delete")
+      .send(member)
+      .expect(422)
+      .expect((res) => {
+        let ret = {
+          ok: false,
+          type: "record delete error",
+          errors: [{msg: "Empty id"}]
+        };
+        expect(res.body).toEqual(ret);
+      })
+      .end((err, res) => {
+        if (err) {
+          console.log(res.body);
+          done(err);
+        }
+        done();
+      });
+    });
+
+    test("idが誤っているdbのイベントレコードの取得に失敗する", (done) => {
+      member.id = "12345";
+      member.memberId = "member0";
+      request(app).post("/member/delete")
+      .send(member)
+      .expect(422)
+      .expect((res) => {
+        let ret = {
+          ok: false,
+          type: "record delete error",
+          errors: [{msg: "missing"}]
+        };
+        expect(res.body).toEqual(ret);
+      })
+      .end((err, res) => {
+        if (err) {
+          console.log(res.body);
+          done(err);
+        }
+        done();
+      });
+    });
+
+    test("memberIdが誤っているため、レコードの取得に失敗する", (done) => {
+      member.id = event_id;
+      member.memberId = "member1";
+      request(app).post("/member/delete")
+      .send(member)
+      .expect(422)
+      .expect((res) => {
+        let ret = {
+          ok: false,
+          type: "record delete error",
+          errors: [{msg: "memberId does not exist"}]
+        };
+        expect(res.body).toEqual(ret);
+      })
+      .end((err, res) => {
+        if (err) {
+          console.log(res.body);
+          done(err);
+        }
+        done();
+      });
+    });
+
+    test("dbのイベントレコード削除中に処理が失敗する", (done) => {
+
+      let model = require("../model/couchdb.js");
+      let spy = jest.spyOn(model.DB.prototype, "updateOneRecord").mockImplementation((currentEvent) => {
+        throw new Error("member update error.");
+      });
+
+      let data = {
+        id: event_id,
+        memberId: "member0"
+      };
+      request(app).post("/member/delete")
+      .send(data)
+      .expect(422)
+      .expect((res) => {
+        expect(res.body.ok).toBe(false);
+        expect(res.body.type).toBe("record delete error");
+      })
+      .end((err, res) => {
+        spy.mockRestore();
+        if (err) {
+          console.log(res);
+          return done(err);
+        }
+        else  {
+          done();
+        }
+      });
+    });
+
+    test("dbのイベントレコードから指定したメンバーの削除に成功する", async () => {
+      // メンバー削除
+      member = {
+        id: event_id,
+        memberId: "member0"
+      };
+      let response = await request(app).post("/member/delete").send(member);
+      expect(response.body.ok).toBe(true);
+
+      // 削除したメンバーチェック
+      let event = {id: event_id};
+      response = await request(app).get("/event/get").query(event);
+      let ret_correct= {}
+      expect(response.res.statusCode).toBe(200);
+      expect(response.body.ok).toBe(true);
+      expect(response.body.eventMembers).toEqual(ret_correct);
+    });
   });
 });
